af3f3426a301d2fcb0be9eedc1e6c962444d718a
root <root@j9u.f.t4vps.eu>
2017-02-04 00:29:56 +0200

    finish building whatanime's API

--- index.js
+++ index.js
@@ -6,14 +6,116 @@ const configs = require('./config.json');
 const whatanime = require('./whatanime/');
 const whatClient = new whatanime(configs.whatanimeToken);
 
+const commands = ['Whatanime'];
+
+const queuedMsgs = [];
+
 /**
- * Log resulted promise
- * @param  {*}       item  'Any type of promise result'
- * @return {Promise}       'Successful Logging'
+ * Join items together
+ * @param {Array} array 'an array of Strings|Numbers'
+ * @return {Array}
  */
-function logResult(item) {
-  console.log(item);
-  return new Promise((res) => {res('Successfully Logged')});
+function joinFields(array) {
+  const text = array.length > 0 ? array.join(', ') : '';
+  return text !== '' ? text : ''
+}
+
+/**
+ * Pad Numbers that are single digit
+ * @param {number} number 'number'
+ * @return {String}
+ */
+function pad(number) {
+  return number < 10 ? `0${number}` : number;
+}
+
+
+/**
+ * Converts time to a more natural looking format
+ * @param {number} seconds 'time in seconds'
+ * @return {String} formatted time
+ */
+function convertTime(seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  seconds = Math.round((seconds - (minutes * 60)) * 1000) / 1000;
+  minutes = Math.round(minutes - (hours * 60));
+  return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+}
+
+/**
+ * Format whatAnime's response and send to the channel.
+ * @param  {Object}  whatAnime 'Object containing the whatAnime response'
+ * @param  {Object}  message   'message Object fromd discord.js'
+ * @return {Promise}
+ */
+function whatResponse(whatAnime, message) {
+  // TODO: anilist.co connection. Use module from npm
+  whatAnime = JSON.parse(whatAnime);
+  const embed = new discord.RichEmbed();
+  const data = whatAnime.docs[0];
+
+  embed
+    .setAuthor('WhatAnime')
+    .setColor('#02a9ff')
+    .setFooter('powered by https://whatanime.ga/')
+    .setThumbnail('https://whatanime.ga/favicon.png')
+    .addField(
+      'Search Time',
+      `${whatAnime.RawDocsSearchTime[0]} milliseconds`
+    )
+    .addField(
+      'Start Time',
+      convertTime(data.from),
+      true
+    )
+    .addField(
+      'End Time',
+      convertTime(data.to),
+      true
+    )
+    .addField(
+      'Episode',
+      data.episode
+    )
+    .addField(
+      'Accuracy',
+      `${data.similarity * 100}%`
+    )
+    .addField(
+      'title',
+      data.title,
+      true
+    )
+    .addField(
+      'English Title',
+      data.title_english,
+      true
+    )
+    .addField(
+      'Romaji Title',
+      data.title_romaji,
+      true
+    )
+
+  // XXX: Special Case; Bad request if field is empty
+  if (joinFields(data.synonyms) !== '') {
+    embed.addField(
+      'Synonyms',
+      joinFields(data.synonyms)
+    );
+  }
+
+  return message.channel.sendEmbed(embed).catch(console.log);
+}
+
+/**
+ * Catch Errors
+ * @param {*} err 'Error'
+ */
+function handleError(err) {
+  console.log('error:');
+  console.log(err);
 }
 
 /**
@@ -21,31 +123,114 @@ function logResult(item) {
  * @param  {Object} linkEmbed 'Embedlink from discord.js'
  * @return {Promise}          'URL of link'
  */
-function tempImage(linkEmbed) {
+function checkForImage(linkEmbed) {
   return new Promise((res, rej) => {
-    if (linkEmbed.type !== 'image') {
-      // TODO WhatanimeAPI
+    if (linkEmbed.type === 'image') {
       res(linkEmbed.url);
-    } else {
+    }
+    else {
       rej(`Requested an image, found ${linkEmbed.type} instead`);
     }
   })
 }
 
+/**
+ * Confirm if prefix is correct
+ * @param  {Object}  message 'message object from discord.js'
+ * @param  {String}  context 'cleaned message string'
+ * @return {Promise}         'command and argument string'
+ */
+function confirmPrefix(message, context) {
+  return new Promise((resolve, reject) => {
+    const prefix = configs.discordPrefix;
+    const validPrefix = context.substring(0, prefix.length);
+    const args = context.substr(prefix.length);
+
+    if (prefix === validPrefix) {
+      resolve({
+        message,
+        args
+      });
+    }
+  })
+}
+
+/**
+ * Queues message for messageEvent to fire
+ * @param {String} messageID 'message string'
+ * @return {Array}           'updated messagequeue array'
+ */
+function queueEmbed(messageID) {
+  return queuedMsgs.push(messageID);
+}
+
+/**
+ * Returns image through chain promising
+ * @param  {Object}  obj 'an object containing a message object'
+ * @return {Promise}
+ */
+function embedResponse(obj) {
+  return checkForImage(obj.message.embeds[0])
+    .then((res) => {
+      return whatClient.findAnime(res)
+    })
+    .then((res) => {
+      return whatResponse(res, obj.message);
+    })
+    .catch(handleError);
+}
+
+/**
+ * Use command with arguments
+ * @param  {Object} obj 'object containing a message and its cleaned text'
+ */
+function activateCommand(obj) {
+  const context = obj.args;
+  const args = context.split(' ');
+  const cmd = args.shift();
+
+  if (commands.indexOf(cmd) !== -1) {
+    switch (cmd) {
+      case 'Whatanime':
+        if (obj.message.embeds.length > 0) {
+          embedResponse(obj);
+        }
+        else {
+          queueEmbed(obj.message.id);
+        }
+        break;
+    }
+  }
+}
+
 client.on('ready', () => {
   console.log('client is ready and is now connected');
-  client.destroy(); // XXX: Temp code for specific testing purposes
+  // client.destroy(); // XXX: Temp code for specific testing purposes
 })
 
 client.on('message', (msg) => {
   /**
    * TODO
-   * 1) Command Check
-   * 2) Download file if embed, checking the embed array (message.embeds > 0, embedItem.url, embItem.type = image) if it's an image that's downloadable and the URL to do so
-   * 3) use whatanime api and upload file to find and respond
-   * 4) return and check validation for any found animes, top 3 results
+   * [X] Command Check
+   * [X] Download file if embed, checking the embed array (message.embeds > 0, embedItem.url, embItem.type = image) if it's an image that's downloadable and the URL to do so
+   * [X] use whatanime api and upload file to find and respond
+   * [ ] return and check validation for any found animes, top 3 results
    * 5) respectively link using anilist.co / mal / some anime site api for easier response
    */
+  confirmPrefix(msg, msg.cleanContent)
+    .then(activateCommand)
 })
+
+client.on('messageUpdate', (oldmsg, newmsg) => {
+  const order = queuedMsgs.indexOf(newmsg.id);
+  if (order !== -1) {
+    embedResponse({
+        message: newmsg
+      })
+      .then(() => {
+        queuedMsgs.splice(order, 1)
+      });
+  }
+});
 
 client.login(configs.discordToken)
--- whatanime/index.js
+++ whatanime/index.js
@@ -1,20 +1,53 @@
 'use strict';
 const request = require('request');
-const requestP = require('request-promise');
+const requestP = require('request-promise'); // TODO delete since unneecsssary in the future
+const sharp = require('sharp');;
 const querystring = require('querystring');
+const fs = require('fs');
 const datauri = require('datauri');
 const whatanimeEndpoint = 'https://whatanime.ga/';
 const temp = './tempImage';
+const tempUpdated = './tempImageUpdated'
+
+// TODO: header check function to prevent unnecessarily large files over X MB
+
+/**
+ * Reduces filesize to maneagable levels
+ * @param {Object} obj 'Object containing an imag file location and apie
+ * @return {Promise}'
+ */
+function reduceSize(obj) {
+  return new Promise((result, reject) => {
+      const file = `${tempUpdated}${Date.now()}.jpg`;
+      setTimeout(() => {
+            sharp(obj.imageFile)
+      .resize(1280, 720)
+      .jpeg({
+        quality: 80
+      })
+      .toFile(file, (err, res) => {
+        if (err) throw err;
+        setTimeout(() => {
+          fs.unlink(obj.imageFile, (err) => {
+            if (err) console.log('could not delete file'); // TODO Consider actual error instead
+          });
+        }, 1000)
+        result({imageFile: file, api: obj.api})
+      })
+    }, 3000) // HACK: Delay time to process image. Started to have issues post 1MB
+  })
+}
 
 /**
  * Download the image to a temporary location
  * @param  {String} imageURL 'Image URL'
  * @return {Promise}         'Temporary file save location if successful'
  */
-function downloadImage(imageURL) {
+function downloadImage(imageURL, api) {
   return new Promise((res, rej) => {
+    const file = `${temp}${Date.now()}.jpg`;
     // TODO double check if file extensions are necessary to use when uploading in wahatanime
-    request(imageURL).pipe(fs.createWriteStream(temp)).on('close', res(temp));
+    request(imageURL).pipe(fs.createWriteStream(file)).on('close', res({imageFile: file, api}));
   })
 }
 
@@ -23,40 +56,50 @@ function downloadImage(imageURL) {
  * @param  {String} imageFile 'File location'
  * @return {Promise}          'JSON response if successful'
  */
-function animeSearch(imageFile) {
-  const data = new datauri(imageFile);
-  const formData = querystring.stringify({image: data.content});
-  const contentLength = formData.length;
-
-  return new Promise((respond, reject) => {
-    request({
-      headers: {
-        'Content-Length': contentLength,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      uri: `${whatanimeEndpoint}/api/search?token=${this.api}`,
-      body: formData,
-      method: 'POST'
-    }, (err, res, body) => {
-      if (err) reject(err);
-      if (res.body.error) reject(res.body.error);
-      if (res.statusCode == 429) {
-        reject(body);
-      };
-      if (res.statusCode == 413) {
-        reject('Filesize Too Big');
-      };
-      if (res.statusCode == 200) {
-        respond(body);
-      }
-    });
+function animeSearch(obj) {
+  // HACK: timeout delay by a full second to allow write to finish. Consider having a read / write / delete in the future
+  return new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        const fileLocation = `${obj.imageFile.replace('./', '')}`;
+        const data = new datauri(fileLocation);
+        const formData = querystring.stringify({image: data.content});
+        const contentLength = formData.length;
+        
+        request({
+          headers: {
+            'Content-Length': contentLength,
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          uri: `${whatanimeEndpoint}/api/search?token=${obj.api}`,
+          body: formData,
+          method: 'POST'
+        },
+        (err, res, body) => {
+        setTimeout(() => {
+          fs.unlink(obj.imageFile, (err) => {
+              if (err) console.log('could not delete file'); // TODO Consider actual error
+            });
+          }, 1000)
+          if (err) reject(err)
+          else if (res.body.error) reject(res.body.error)
+          else if (res.statusCode == 429) {
+            reject(body);
+          }
+          else if (res.statusCode == 413) {
+            reject('Filesize Too Big');
+          }
+          else if (res.statusCode == 200) {
+            resolve(body);
+          }
+        });
+      }, 1000); // HACK: originally before image filesize reducing, 
   });
 }
 
 class whatanime {
   constructor(api) {
     this.api = api;
-    this.whatanime = 'https://whatanime.ga/'
+    this.whatanime = 'https://whatanime.ga/';
   }
 
   /**
@@ -77,6 +120,9 @@ class whatanime {
    */
   findAnime(imageURL) {
     // TODO create image search
+    return downloadImage(imageURL, this.api)
+    .then(reduceSize)
+    .then(animeSearch);
   }
 }
 
